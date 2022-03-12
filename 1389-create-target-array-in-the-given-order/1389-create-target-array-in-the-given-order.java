090c3ffd57d0b6175888f55e9bd0dc13ce8445c8
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-03-11 23:02:25 -0500

    Time: 1 ms (78.54%), Space: 42.4 MB (41.31%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        // If there is only one element, return early.
-        if(index.length == 1 ){
-            return nums;
-        }
-        
-        int[] retArray = new int[nums.length];
-        ArrayList tempArr = new ArrayList();
-        
-        // Take advantage of ArrayList.add() so we don't have to
-        // manually reorder array elements on each add().
-        for(int i = 0;i<nums.length;i++){
-            tempArr.add(index[i],nums[i]);
-        }
-        
-        // Fill our int[] with the elements in our ArrayList
-        for(int k = 0;k<tempArr.size();k++){
-            // Because ArrayList.get() returns Object type, we have
-            // type cast to an integer.
-            retArray[k] = (Integer)tempArr.get(k);
-        }
-        return retArray;
+        int[] target = new int[nums.length];
+		int count = 0;
+		while (count < target.length) {
+            if (index[count] >= count) {
+				target[index[count]] = nums[count];
+			}
+            else{
+				for (int i = target.length - 1; i > index[count]; i--)
+					target[i] = target[i - 1];
+				target[index[count]] = nums[count];
+			}
+			count++;
+		}
+        return target;
     }
 }
