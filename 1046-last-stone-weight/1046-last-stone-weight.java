1b7033eab90c9aaf4ccae7be62bfec084fea9329
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-03-15 15:11:11 -0400

    Time: 2 ms (75.69%), Space: 40.7 MB (60.22%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,20 +1,21 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
      
-        
-        while(stones.length > 1){
+     
+        while(stones.length-1>0){
             Arrays.sort(stones);
-            int sum = stones[stones.length-1] - stones[stones.length-2];
-            
-            if(sum >0){
+            int smash = stones[stones.length-1] - stones[stones.length-2];
+            if(smash != 0){
                 stones = Arrays.copyOf(stones,stones.length-1);
-                stones[stones.length-1] = sum;
+                stones[stones.length-1] = smash; 
             }
             else{
                 stones = Arrays.copyOf(stones,stones.length-2);
             }
+           
         }
         
-        return stones.length == 0 ? 0 : stones[0];
+        return stones.length == 0? 0: stones[0];
+   
     }
 }
