de411ddd4599b53cf28ffd4a4492a25e5705184b
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-03-30 00:39:18 -0400

    Time: 2 ms (88.72%), Space: 40.4 MB (96.85%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean areOccurrencesEqual(String s) {
-        
-        int[] alpha = new int[26];
-        for(char c : s.toCharArray()){
-              System.out.println(c-'a');
-            alpha[c-'a']++;
+	//create an array to track frequencies of characters
+        int[] trackerArray = new int[26];
+        for(int i=0;i<s.length();i++){
+            trackerArray[s.charAt(i)-'a']++;
+        }
+ //if the min and max occurrences of the characters are different, we return false.
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<trackerArray.length;i++){
+		// Since the array was initialized with 0 values, we only check indexes with non zero position
+            if(trackerArray[i]>0){
+            min = Math.min(trackerArray[i],min);
+            max = Math.max(trackerArray[i],max);
+        }
         }
         
-       int max = Arrays.stream(alpha).max().getAsInt();
-        for(int i = 0 ; i < 25; i++){
-            
-            if(alpha[i] != 0 && alpha[i] != max)
-                return false;
-        }
-        
-        return true;
+        return min == max;
     }
 }
