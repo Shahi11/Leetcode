493bbd1fd9ac8ed0c8efda39c1feea208564bb0c
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-04-04 01:02:34 -0400

    Time: 1 ms (50.22%), Space: 49.6 MB (31.56%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,16 +1,30 @@
 class Solution {
     public int countNegatives(int[][] grid) {
-        
-        int count = 0;
-        
-        for(int i = grid.length-1; i >= 0; i--){
-            int j = grid[0].length-1;
-            while( j >=0 && grid[i][j] < 0){
-                count++;
-                j--;
+        int rows = grid.length, cols = grid[0].length; 
+        int res = 0, lastNeg = cols - 1;
+        for (int row = 0; row < rows; row++) {
+            //check edge cases - if first element is < 0 - all elements in row are negative
+            if (grid[row][0] < 0) {
+                res+=cols;
+                continue;
             }
+            //if last element is positive - it means there are no negative numbers in a row
+            if (grid[row][cols - 1] > 0)
+                continue;
+            //there is a mix of negative and positive ones, need to find the border. starting
+            //binary search
+            int l = 0, r = lastNeg;
+            while (l <= r) {
+                int m = l + (r - l)/2;
+                if (grid[row][m] < 0) {
+                    r = m - 1;
+                } else
+                    l = m + 1;
+            }
+            //l points to the first negative element, which means cols - l is a number of
+            //such elements
+            res += (cols - l); lastNeg = l;
         }
-        
-        return count;
+        return res;
     }
 }
