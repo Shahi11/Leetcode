094a09b612072b17dfc05be75e421b145a4abce3
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-04-04 01:07:58 -0400

    Time: 0 ms (100.00%), Space: 40.9 MB (66.05%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -1,23 +1,15 @@
 class Solution {
     public int countOperations(int num1, int num2) {
-        if(num1 ==0 || num2 ==0) return 0;
-        int count = 0;
-        while(num1 != 0){
-            if(num1 > num2){
-                num1 -= num2;
-                count++;
+        int count=0;
+        while(num1!=0 && num2!=0){
+            if(num1<num2){
+                count+=num2/num1;
+                num2=num2%num1;
+            }else{
+                count+=num1/num2;
+                num1=num1%num2;
             }
-             else if(num1 < num2){
-                num2 -= num1;
-                count++;
-            }
-            else{
-                num1 -= num2;
-                count++;
-            }
-                
         }
-            
         return count;
     }
 }
