4e702198958e2dd41fb567100e1fff0ab1ac2088
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-05-31 23:10:19 -0500

    Time: 0 ms (100.00%), Space: 41.5 MB (7.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,20 +1,19 @@
 class Solution {
-         
-     
     public int climbStairs(int n) {
-    int first = 1, second =1, sum = 0;
-        
-    if(n <=2) return n;
-        
+        int[] res = new int[n+1];
+        res[0]=1;
+        res[1]=1;
         for(int i = 2; i <= n ; i++){
-            sum = first +second;
-            first = second;
-            second = sum;
-            
+            res[i] = res[i-1] + res[i-2];
         }
         
-        return sum;
-            
+        return res[n];
+        
     }
-     
 }
+
+// 1 = 1
+// 2 = 1,2 = 2
+// 3 = 111,12,21 = 3
+// 4 = 1111,22,121,211,112 =   5  
+// 5 =  11111,221,212,122,1112,1121,1211,2111 = 8   
