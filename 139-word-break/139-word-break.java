2095561d2404c94d7b666183067f17e69a756574
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-06-03 22:58:34 -0500

    Time: 7 ms (68.58%), Space: 41.9 MB (94.08%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,16 +1,19 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        
         int len = s.length();
-        boolean[] dp = new boolean[len+1];
+        
         int max = 0;
-        for(String word : wordDict){
-            max = Math.max(max,word.length());
-        }
+        for(String ss : wordDict)
+          max = Math.max(ss.length(), max);
+        
+        boolean[] dp = new boolean[len+1];
         dp[0] = true;
-        for(int i = 0 ; i<=len; i++){
-            for(int j = 0 ; j < i; j++){
+        for(int i = 0; i <= len; i++){
+            for(int j = 0; j < i; j++){
                 if(i-j > max) continue;
-                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                if(dp[j] && wordDict.contains(s.substring(j,i)))
+                {
                     dp[i] = true;
                     break;
                 }
