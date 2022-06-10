6bf24dc1cc85b3b1bd191a620b7e66d8090c52ec
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-06-10 16:10:01 -0500

    Time: 22 ms (29.89%), Space: 51.8 MB (7.61%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
         int max = 0;
-        for(int i = 0 ; i < nums.length; i++){
-            int freq = map.getOrDefault(nums[i],0)+1;
-            map.put(nums[i],freq);
-            max = Math.max(max,freq);
+        for(int i : nums){
+                map.put(i, map.getOrDefault(i,0)+1);
+             max = Math.max(max, map.get(i));
         }
         
-        List<Integer>[] ls = new List[max+1];
+        ArrayList<Integer>[] ls = new ArrayList[max+1];
         
-        for(int key : map.keySet()){
-            int freq = map.get(key);
+        for(int i : map.keySet()){
+            int freq = map.get(i);
             if(ls[freq] == null)
                 ls[freq] = new ArrayList<>();
-            ls[freq].add(key);
+            ls[freq].add(i);
         }
         
-        List<Integer> ans = new ArrayList<>();
-        for(int j = ls.length-1; j>0 && k>0;j--){
-            if(ls[j] != null){
-                List<Integer> tmp = ls[j];
-                ans.addAll(tmp);
-                k=k-tmp.size();
+        List<Integer> res = new ArrayList<>();
+        for(int i = ls.length-1; i >= 0 && k >0; i--){
+            if(ls[i] != null){
+                //List<Integer> tmp = ls[i];
+                res.addAll(ls[i]);
+                k = k - ls[i].size();
             }
         }
         
-        return ans.stream().mapToInt(i->i).toArray();
+        return res.stream().mapToInt(i->i).toArray();
     }
 }
