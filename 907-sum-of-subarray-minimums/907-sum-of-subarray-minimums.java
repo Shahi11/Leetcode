691a307dd6a14910449433228f1a67c06de8047a
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-06-14 19:51:20 -0500

    Time: 46 ms (73.74%), Space: 71.8 MB (21.82%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,24 +1,54 @@
-class Solution {
-    public int sumSubarrayMins(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
-        int n = arr.length;
-        int[] leftMin = new int[n];
-        Arrays.fill(leftMin,-1);
-        int[] rightMin = new int[n];
-        Arrays.fill(rightMin,n);
-        for(int i=0;i<n;i++) {
-            int curele = arr[i];
-            while(!stack.isEmpty() && arr[stack.peek()] >=curele ) {
-                rightMin[stack.pop()] = i;
-            }
-            if(!stack.isEmpty()) leftMin[i] = stack.peek();
-            stack.push(i);
-        }
-        long res = 0, mod = (int)1e9 + 7;
-        for(int i = 0;i<n;i++){
-           
-            res = (res +  (long) Math.abs(i-leftMin[i]) * Math.abs(i-rightMin[i]) * arr[i] ) % mod;
-        }
-        return (int) res;
+class pair{
+    int num;
+    int count;
+    pair(int n, int c){
+        num = n;
+        count = c;
     }
 }
+
+class Solution {
+    
+     public int mod = (int)Math.pow(10,9)+7;
+    public int sumSubarrayMins(int[] arr) {
+        
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+       
+        
+        ArrayDeque<pair> q1 = new ArrayDeque<>();
+        ArrayDeque<pair> q2 = new ArrayDeque<>();
+        
+        for(int i = 0 ; i < n; i++){
+            int cur = arr[i];
+            int cnt = 1;
+            
+            while(q1.size() != 0 && q1.peek().num > cur){
+                cnt += q1.pop().count;
+            }
+            q1.push(new pair(cur,cnt));
+            left[i] = cnt;
+        }
+        
+        for(int i = n-1; i >= 0 ; i--){
+            int cur = arr[i];
+            int cnt=1;
+            
+            while(q2.size() != 0 && q2.peek().num >= cur){
+                cnt += q2.pop().count;
+            }
+            q2.push(new pair(cur,cnt));
+            right[i] = cnt;
+        }
+        
+        
+        long res = 0, mod = (int)Math.pow(10,9)+7;
+        for (int i = 0; i < n; i++)
+            res = (res + (long)arr[i] * left[i] * right[i] ) % mod;
+        return (int)res;
+    }
+}
+
+
+// No. of subarrays of n length = n (n+1) /2
