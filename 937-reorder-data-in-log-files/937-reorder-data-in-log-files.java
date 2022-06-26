184f9a82dd275eacaa4f06f1254e8525ed9478cb
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-06-25 20:06:00 -0500

    Time: 8 ms (65.19%), Space: 46.6 MB (58.49%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,29 +1,34 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        Arrays.sort(logs,(a,b) -> {
-           // log1 < log2 -1
-            // log1 == log2 0
-            //log1 > log 2 1
-            int index1 = a.indexOf(" ");
-            String id1 = a.substring(0,index1);
-            String main1 = a.substring(index1+1);
+        
+        Arrays.sort(logs,(a,b) ->  {
             
-            int index2 = b.indexOf(" ");
-            String id2 = b.substring(0,index2);
-            String main2 = b.substring(index2+1);
+            //log  a > log b -1
+            // log a = log b 0
+            // log a < log b 1
+            int ind1 = a.indexOf(" ");
+            String id1 = a.substring(0,ind1);
+            String main1 = a.substring(ind1+1);
+                        
+            int ind2 = b.indexOf(" ");
+            String id2 = b.substring(0,ind2);
+            String main2 = b.substring(ind2+1);
             
             boolean isDigit1 = Character.isDigit(main1.charAt(0));
             boolean isDigit2 = Character.isDigit(main2.charAt(0));
             
             if(!isDigit1 && !isDigit2){
                 int diff = main1.compareTo(main2);
-                if(diff == 0) return id1.compareTo(id2);
+                if(diff == 0 )
+                    return id1.compareTo(id2);
                 return diff;
             }
             
-            return isDigit1 ? (isDigit2 ? 0 : 1) : -1; 
+            return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+            
             
         });
+        
         
         return logs;
     }
