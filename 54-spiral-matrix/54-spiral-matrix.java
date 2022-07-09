695d4e1aff406aaac5b3734258cd0c594970fc6a
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-07-09 17:48:06 -0500

    Time: 0 ms (100.00%), Space: 42.3 MB (26.58%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,33 +1,38 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
         
         int row = matrix.length;
         int col = matrix[0].length;
-        int l = 0, r = matrix[0].length-1, t = 0, b = matrix.length-1;
+        
+        int t= 0, l =0, b = row-1, r = col-1; 
+        List<Integer> res = new ArrayList<>();
         
         while(res.size() < row*col){
-            for(int i = l; i <= r; i++)
+            for(int i = l; i <= r; i++){
                 res.add(matrix[t][i]);
-            
-          
+            }
+
             for(int i = t+1; i <= b; i++)
                 res.add(matrix[i][r]);
             
-          
-            if(t != b){
-            for(int i = r-1; i >= l; i--)
-                res.add(matrix[b][i]);
+ 
+            
+            if(t != b ){
+                for(int i = r-1; i >= l; i--)
+                    res.add(matrix[b][i]);
             }
             
             if(l != r){
-            for(int i = b-1; i > t; i--)
-                res.add(matrix[i][l]);
+                for(int i = b-1; i > t; i--)
+                    res.add(matrix[i][l]);
             }
-            l++;
+            
             t++;
             r--;
             b--;
+            l++;
+                
+            
         }
         
         return res;
