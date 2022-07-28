ca374348367ffa2f17c2a87830d9f4309e4473ea
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-07-27 22:57:44 -0500

    Time: 3 ms (97.92%), Space: 58.2 MB (11.39%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,36 +1,32 @@
 class Solution {
-    
-   
-    int row;
-    int col;
+    int row, col;
     public int numIslands(char[][] grid) {
+        
         row = grid.length;
         col = grid[0].length;
-        int count =0;
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j< col; j++){
-            if(grid[i][j] == '1'){
-                countIsland(i,j,grid);
-                count++;
+        int count = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    checkIsland(i,j,grid);
+                }
             }
         }
         
-      
-    }  
-          return count;
+        return count;
         
     }
     
-    public void countIsland(int r, int c, char[][] grid){
+    public void checkIsland(int r, int c, char[][] grid){
         
-        if(r<0 || r >= row || c < 0 || c >= col || grid[r][c] == '0')
+        if(r < 0 || r >= row || c < 0 || c >= col || grid[r][c] == '0')
             return;
         
         grid[r][c] = '0';
-        
-        countIsland(r+1,c,grid);
-        countIsland(r-1,c,grid);
-        countIsland(r,c+1,grid);
-        countIsland(r,c-1,grid);
+        checkIsland(r+1, c, grid);
+        checkIsland(r-1, c, grid);
+        checkIsland(r, c+1, grid);
+        checkIsland(r, c-1, grid);
     }
 }
