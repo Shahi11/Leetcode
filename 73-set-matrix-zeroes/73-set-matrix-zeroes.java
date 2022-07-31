cfe6c7203890899579aa841bb1e8af6a34342c42
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-07-31 14:50:52 -0500

    Time: 1 ms (97.36%), Space: 54.5 MB (20.79%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,28 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         
-        int r = matrix.length;
-        int c = matrix[0].length;
+        int row = matrix.length;
+        int col = matrix[0].length;
         
-        int fr = 1, fc = 1;
+        boolean rowc = false;;
+        boolean colc = false;
         
-        for(int i = 0; i < r; i++){
+        for(int i = 0; i < row; i++){
             if(matrix[i][0] == 0){
-                fc = 0;
-                break;
+                rowc = true;
+            break;
             }
         }
         
-       for(int i = 0; i < c; i++){
+        for(int i = 0 ; i < col; i++){
             if(matrix[0][i] == 0){
-                fr = 0;
+                colc = true;
                 break;
             }
         }
         
         
-        for(int i = 0 ; i < r ; i++){
-            for(int j = 0; j < c; j++){
+        for(int i = 1 ; i < row; i++){
+            for(int j = 1 ; j < col; j++){
                 if(matrix[i][j] == 0){
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
@@ -30,24 +31,25 @@ class Solution {
             }
         }
         
-        for(int i = 1; i < r; i++){
-            for(int j = 1; j < c; j++){
-                if(matrix[0][j] == 0 || matrix[i][0] == 0)
-                    matrix[i][j] =0;
-            }
+        for(int i = 1; i < row; i++){
+             for(int j = 1 ; j < col; j++){
+                 if(matrix[i][0] ==0 || matrix[0][j] == 0)
+                     matrix[i][j] = 0;
+             }
         }
         
-        if(fc == 0){
-            for(int i = 0; i<r; i++)
+        
+        if(rowc){
+            for(int i = 0 ; i < row; i++)
                 matrix[i][0] = 0;
         }
         
-        if(fr == 0){
-            for(int i = 0; i < c; i++){
+        if(colc){
+            for(int i = 0 ; i < col; i++)
                 matrix[0][i] = 0;
-            }
         }
             
+        
         
     }
 }
