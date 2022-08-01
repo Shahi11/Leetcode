54379452c634e1985a770325269443312054bc6f
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-07-31 21:42:26 -0500

    Time: 2 ms (8.99%), Space: 44.1 MB (5.38%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -13,27 +13,44 @@
  *     }
  * }
  */
+
+class tuple{
+    int level;
+    TreeNode n;
+    
+    tuple(int level, TreeNode n){
+        this.level = level;
+        this.n = n;
+    }
+}
 class Solution {
-    int max;
     public int maxDepth(TreeNode root) {
-        max = 0;
-        recursion(root, 1);
+        
+        if(root == null)
+            return 0;
+        
+        int max = 1;
+        
+        Queue<tuple> q = new LinkedList<>();
+        q.add(new tuple(1,root));
+        
+        while(!q.isEmpty()){
+            tuple t = q.remove();
+            
+            if(t.n.left != null){
+                q.add(new tuple(t.level+1,t.n.left));
+                max = Math.max(max, t.level+1);
+            }
+            
+            if(t.n.right != null){
+                q.add(new tuple(t.level+1,t.n.right));
+                max = Math.max(max, t.level+1);
+            }
+            
+        }
         
         return max;
-    }
-    
-    public void recursion(TreeNode node, int level){
         
-        if(node == null)
-            return;
-        
-        max = Math.max(max, level);
-        
-        if(node.left != null)
-            recursion(node.left, level+1);
-        
-        if(node.right != null)
-            recursion(node.right, level+1);
         
     }
 }
