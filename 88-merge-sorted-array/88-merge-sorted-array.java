d5a995eff9441bb8771aa7d0ed6bdd74fc48e975
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-01 23:27:52 -0500

    Time: 0 ms (100.00%), Space: 42.8 MB (49.87%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,12 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int end1 = m-1, end2 = n-1, end3 = m+n-1;
-        while(end1 >= 0 && end2 >= 0){
-            nums1[end3--] = nums1[end1]>=nums2[end2] ? nums1[end1--] : nums2[end2--];
+        
+        int e1 = m-1, e2 = n-1, e3 = m+n -1;
+        while(e1>= 0 && e2 >= 0){
+            nums1[e3--] = nums1[e1] > nums2[e2]? nums1[e1--] : nums2[e2--];
         }
         
-        while (end2 >= 0){
-            nums1[end3--] = nums2[end2--];
-        }
+        while(e2 >= 0)
+            nums1[e3--] = nums2[e2--];
     }
 }
