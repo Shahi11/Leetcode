ff29fb08e425bb9ab15f263e2550f89ae6d32af7
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-03 22:03:26 -0500

    Time: 0 ms (100.00%), Space: 51.6 MB (38.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.java
@@ -13,16 +13,14 @@
  *     }
  * }
  */
-
 class Solution {
-	public TreeNode searchBST(TreeNode root, int val) {
-		while (root != null)
-		{
-			if ( val < root.val ) root = root.left;
-			else if ( val > root.val ) root = root.right;
-			else return root;
-		}
-		return root;
-	}
-
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        while(root != null && root.val != val){
+            root = val < root.val? root.left : root.right;
+        }
+        
+        return root;
+        
+    }
 }
