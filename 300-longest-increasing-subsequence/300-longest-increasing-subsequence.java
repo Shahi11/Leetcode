f619cf656190c8f3cfcb09ef4ee150619bf10267
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-07 18:20:44 -0500

    Time: 74 ms (55.96%), Space: 44 MB (74.10%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,19 +1,22 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         
+        int max = 0;
+        if(nums.length <= 1)
+            return nums.length;
+        
         int[] dp = new int[nums.length];
         Arrays.fill(dp,1);
-        
-        for(int i = 1; i< nums.length; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j])
-                    dp[i]  = Math.max(dp[i],dp[j]+1);
+        for(int i = 1; i < nums.length; i++){
+            for(int j = 0; j < i ; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                   
+                }
+                 max = Math.max(max, dp[i]);
             }
         }
         
-        return Arrays.stream(dp).max().getAsInt();
-        
+        return max;
     }
 }
-
-// DP question: every dp we store how many previous element is it greater than
