ccbf1292a1d72c5c653c41f38530823584e926b7
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-07 15:01:33 -0500

    Time: 11 ms (82.77%), Space: 55.5 MB (35.14%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,17 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+      
+     Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
-        LinkedList<int[]> res = new LinkedList<>();
-        for(int[] interval : intervals){
-            if(res.size() == 0 || res.getLast()[1] < interval[0])
-                res.add(interval);
-            else{
-                res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
-            }
+     LinkedList<int[]> ls = new LinkedList<>();
+        
+    for(int[] interval: intervals){
+        
+        if(ls.isEmpty() || ls.getLast()[1] < interval[0]){
+            ls.add(interval);
         }
+        else{
+            ls.getLast()[1] = Math.max(ls.getLast()[1], interval[1]);
+            
+        }
+    }
+        
+        return ls.toArray(new int[ls.size()][]);
         
         
-        return res.toArray(new int[res.size()][2]);
     }
 }
+
+//T: O(logn) + O(n)
