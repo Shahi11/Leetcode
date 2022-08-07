3978e6bb0d712baa63b9d58f5c1c1375f1356a9e
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-07 15:11:55 -0500

    Time: 3 ms (84.77%), Space: 46.9 MB (75.41%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,29 +1,33 @@
 class Solution {
-    boolean[][] visited;
+    int r, c;
+    
     public int maxAreaOfIsland(int[][] grid) {
-        int ar=0;
-        visited = new boolean[grid.length][grid[0].length];
         
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                ar = Math.max(ar,area(i,j,grid));
+        r = grid.length;
+        c = grid[0].length;
+        
+        int count = 0;
+        for(int i = 0 ; i < r; i++){
+            for(int j = 0 ; j < c; j++){
+                if(grid[i][j] == 1)
+                    count = Math.max(count, check(i,j,grid));
             }
         }
         
-        return ar;
+        return count;
+        
     }
     
     
-    public int area(int i, int j, int[][] grid){
+    public int check(int i, int j, int[][] grid){
         
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] 
-          || grid[i][j] == 0)
+        if(i < 0 || i >= r || j < 0 || j >= c || grid[i][j] == 0)
             return 0;
         
-        visited[i][j] = true;
-        
-        return(1+area(i+1,j,grid)+area(i-1,j,grid)+area(i,j+1,grid)+area(i,j-1,grid));
-        
-        
+        grid[i][j] = 0;
+        return (1+ check(i+1,j,grid) +
+        check(i-1,j,grid) +
+        check(i,j+1,grid) +
+        check(i,j-1,grid) );
     }
 }
