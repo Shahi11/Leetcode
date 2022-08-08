d941ee27e3b9e2927aa0e91480957ab828beacf9
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-07 20:40:46 -0500

    Time: 20 ms (59.25%), Space: 45.9 MB (89.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,22 +1,22 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         
-        int l1 = text1.length();
+      int l1 = text1.length();
         int l2 = text2.length();
+        int[][] dp = new int[l1+1][l2+1];
         
-        int dp[][] = new int[l1+1][l2+1];
-        
-        for(int i = 0; i <= l1; i++){
-            for(int j = 0; j <= l2; j++){
-                if(i == 0 || j == 0)
+        for(int i = 0 ; i <= l1; i++){
+            for(int j = 0; j<= l2; j++){
+                if(i ==0 || j ==0)
                     dp[i][j] = 0;
+                
                 else if(text1.charAt(i-1) == text2.charAt(j-1))
-                    dp[i][j] = 1+ dp[i-1][j-1];
-                else
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = dp[i-1][j-1]+1;
+                else{
+                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                }
             }
         }
-        
         
         return dp[l1][l2];
         
@@ -24,4 +24,7 @@ class Solution {
 }
 
 
-//Recursion: check each index of both string and move ahead;
+//Recursion: check each index of both string and move ahead; 2^max(m,n)
+
+//Time complexity: m*n
+//space complexity: m*n
