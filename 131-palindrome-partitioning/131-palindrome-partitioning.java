f3565ca3f460b85eb751f4de797cbfc19f1b1288
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-08-09 00:17:57 -0500

    Time: 15 ms (79.93%), Space: 195.8 MB (30.95%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -3,32 +3,36 @@ class Solution {
         
         List<List<String>> res = new ArrayList<>();
         
-        helper(0, s,res, new ArrayList<>());
+        rec(0, s, new ArrayList<>(), res);
+        
         return res;
+        
     }
     
-    public void helper(int index, String s, List<List<String>> res, List<String> cur){
-        if(s.length() == index){
+    public void rec(int index, String s, List<String> cur, List<List<String>> res){
+        
+        if(index == s.length()){
             res.add(new ArrayList<>(cur));
             return;
         }
         
-        for(int i = index; i< s.length(); i++){
-            if(isPallindrome(s,index,i)){
-                cur.add(s.substring(index,i+1));
-                helper(i+1,s,res,cur);
+        for(int i = index; i < s.length(); i++){
+            if(checkPall(s, index, i)){
+                cur.add(s.substring(index, i+1));
+                rec(i+1, s, cur, res);
                 cur.remove(cur.size()-1);
             }
         }
-        
     }
     
-    public boolean isPallindrome(String s, int start, int end){
-        while(start <= end){
-            if(s.charAt(start++) != s.charAt(end--))
+    public boolean checkPall(String s, int st, int e){
+        
+        while(st <= e){
+            if(s.charAt(st++) != s.charAt(e--))
                 return false;
         }
         
         return true;
+            
     }
 }
