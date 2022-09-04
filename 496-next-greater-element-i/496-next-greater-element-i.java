0640c8f099455aa39bf70212d3c76b5138d61d3d
Rahul Shahi <89864965+Shahi11@users.noreply.github.com>
2022-09-04 15:48:33 -0400

    Time: 9 ms (17.71%), Space: 44.9 MB (12.49%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Stack<Integer> stack = new Stack();
         
-        for(int i : nums2){
-            while(!stack.isEmpty() && stack.peek() < i)
-                map.put(stack.pop(),i);
+        
+        int[] nxt = new int[nums2.length];
+        Arrays.fill(nxt,-1);
+        Stack<Integer> st = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums2.length; i++){
             
-            stack.push(i);
+            while(!st.isEmpty() && st.peek() < nums2[i]){
+                nxt[map.get(st.peek())] = nums2[i];
+                st.pop();
+            }
+            map.put(nums2[i], i);
+            st.push(nums2[i]);
         }
+        
+        int[] res = new int[nums1.length];
         
         for(int i = 0 ; i < nums1.length; i++){
-            nums1[i] = map.getOrDefault(nums1[i],-1);
+            res[i] = nxt[map.get(nums1[i])];
         }
         
-        return nums1;
+        
+        return res;
+        
+        
     }
 }
